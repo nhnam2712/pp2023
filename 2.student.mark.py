44ed2ec55c8a2dbb46ee2aaa6bcfd3b89df948f9
Nam Nguyễn <nguyennam@Nams-MacBook-Pro.local>
2023-03-09 12:01:14 +0700

    add encapsulation for prac2

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,25 +1,63 @@
 #create class for students and courses
 class Student:
     def __init__(self, name, id, dob):
-        self.name = name
-        self.id = id
-        self.dob = dob
+        self.__name = 0
+        self.__id = 0
+        self.__dob = 0
+
+    def _get_name(self):
+            return self.__name
+
+    def set_name(self, name):
+            self.__name = name
+
+    def _get_id(self):
+            return self.__id
+
+    def set_id(self, id):
+            self.__id = id
+
+    def _get_dob(self):
+            return self.__dob
+
+    def set_dob(self, dob):
+            self.__dob = dob
+
+
 
 class Course:
     def __init__(self, name, id):
-        self.name = name
-        self.id = id
-        self.mark = {}
+        self.__name = 0
+        self.__id = 0
+        self.__mark = {}
+
+    def _get_name(self):
+            return self.__name
+
+    def set_name(self, name):
+            self.__name = name
+
+    def _get_id(self):
+            return self.__id
+
+    def set_id(self, id):
+            self.__id = id
+
+    def _get_mark(self):
+            return self.__mark
+
+    def set_mark(self, mark):
+            self.__mark = mark
 
 #define marks
     def input_mark(self, students):
-        print(f"Input mark for course {self.name}")
+        print(f"Input mark for course {self.__name}")
         for student in students:
-            mark = input(f"* Student {student.name}: ")
-            self.mark[student.id] = mark
+            mark = input(f"* Student {student.__name}: ")
+            self.__mark[student.id] = mark
 
     def show_mark(self):
-        for student_id, mark in self.mark.items():
+        for student_id, mark in self.__mark.items():
             print(f"Student {student_id} has mark {mark}")
 
 #create dict for students and courses
@@ -46,11 +84,11 @@ for course in courses:
 
 print("listing students")
 for student in students:
-    print(student.name, student.id, student.dob)
+    print(student._get_name(), student._get_id(), student._get_dob())
 
 print("listing courses")
 for course in courses:
-    print(course.name, course.id)
+    print(course._get_name(), course._get_id())
 
 print("show mark:")
 for course in courses:
